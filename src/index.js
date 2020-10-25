7e3653131a4df6453bb2d95656fdace38ddfe6db
odinnmk <odie1ne@gmail.com>
2020-10-25 15:52:40 +0000

    Message

--- src/index.js
+++ src/index.js
@@ -1,12 +1,15 @@
-import React from "react";
-import ReactDOM from "react-dom";
+import React, {Component} from 'react'
+import ReactDOM from 'react-dom'
+import './css/foo.css'
 
-import App from "./App";
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <h1>Hello, React!</h1>
+      </div>
+    )
+  }
+}
 
-const rootElement = document.getElementById("root");
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  rootElement
-);
+ReactDOM.render(<App />, document.getElementById('root'))
